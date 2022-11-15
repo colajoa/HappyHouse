b8a7a6c00658dac13e36de0acf0779f3edeb3c6f
jungeun <kjjee99@naver.com>
2022-11-15 15:36:12 +0900

    [Feat] DAO 구현

--- happyhouse/src/main/java/com/ssafy/happyhouse/dao/QnaDao.java
+++ happyhouse/src/main/java/com/ssafy/happyhouse/dao/QnaDao.java
@@ -1,5 +1,17 @@
 package com.ssafy.happyhouse.dao;
 
-public class QnaDao {
-    
+import java.util.List;
+
+import com.ssafy.happyhouse.dto.QnaDto;
+
+public interface QnaDao {
+    int insertQna(QnaDto post);
+
+	List<QnaDto> listQna();
+
+	QnaDto detailQna(int id);
+
+	int updateQna(QnaDto post);
+
+	int deleteQna(int id);
 }
--- happyhouse/src/main/java/com/ssafy/happyhouse/dao/UserDao.java
+++ happyhouse/src/main/java/com/ssafy/happyhouse/dao/UserDao.java
@@ -1,5 +1,17 @@
 package com.ssafy.happyhouse.dao;
 
+import com.ssafy.happyhouse.dto.UserDto;
+
 public interface UserDao {
-    
+    public UserDto getLoginUser(UserDto user);
+
+	public int insertUser(UserDto user);
+
+	public int updateUser(UserDto user);
+
+	public int deleteUser(UserDto user);
+
+	public UserDto findPwd(UserDto user);
+
+	public int idCheck(UserDto user);
 }
