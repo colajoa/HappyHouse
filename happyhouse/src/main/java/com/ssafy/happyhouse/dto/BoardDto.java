6a1235181fac94274a11ea2c9a654b8db38fff4e
jungeun <kjjee99@naver.com>
2022-11-15 15:28:12 +0900

    [Feat] DTO 구현

--- happyhouse/src/main/java/com/ssafy/happyhouse/dto/BoardDto.java
+++ happyhouse/src/main/java/com/ssafy/happyhouse/dto/BoardDto.java
@@ -1,5 +1,27 @@
 package com.ssafy.happyhouse.dto;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class BoardDto {
+
+    private int id;
+	private String title;
+	private String writer;
+	private String content;
+	private String createdAt;
+
+	@Builder
+	public BoardDto(int id, String title, String writer, String content, String createdAt) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.writer = writer;
+		this.content = content;
+		this.createdAt = createdAt;
+	}
     
 }
