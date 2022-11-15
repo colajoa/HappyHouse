0b6d4edcdf1b5b89baad28c78a8b257b82453c64
jungeun <kjjee99@naver.com>
2022-11-15 17:09:27 +0900

    [Fix] Mapper Annotation 추가

--- happyhouse/src/main/java/com/ssafy/happyhouse/dao/QnaDao.java
+++ happyhouse/src/main/java/com/ssafy/happyhouse/dao/QnaDao.java
@@ -2,8 +2,11 @@ package com.ssafy.happyhouse.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.ssafy.happyhouse.dto.QnaDto;
 
+@Mapper
 public interface QnaDao {
     int insertQna(QnaDto post);
 
--- happyhouse/src/main/java/com/ssafy/happyhouse/dao/UserDao.java
+++ happyhouse/src/main/java/com/ssafy/happyhouse/dao/UserDao.java
@@ -1,7 +1,10 @@
 package com.ssafy.happyhouse.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.ssafy.happyhouse.dto.UserDto;
 
+@Mapper
 public interface UserDao {
     public UserDto getLoginUser(UserDto user);
 
