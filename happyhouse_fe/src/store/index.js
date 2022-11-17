9a839c03c1765ec480bd593eb3f9b389a3955a30
jungeun <kjjee99@naver.com>
2022-11-17 13:24:43 +0900

    [Feat] Add AptStore module

--- happyhouse_fe/src/store/index.js
+++ happyhouse_fe/src/store/index.js
@@ -1,12 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+// modules
+import aptStore from "@/store/modules/AptStore.vue";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: {
+    aptStore,
+  },
+  plugins: [
+    createPersistedState({
+      storage: sessionStorage,
+    }),
+  ],
 });
