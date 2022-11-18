2441ca9a5f16eed19ef05e9ebdb3db31a915b8b5
jungeun <kjjee99@naver.com>
2022-11-18 15:15:49 +0900

    [Feat] Login 구현

--- happyhouse_fe/src/components/user/UserLogin.vue
+++ happyhouse_fe/src/components/user/UserLogin.vue
@@ -14,6 +14,7 @@
         class="form-control"
         id="floatingInput"
         placeholder="아이디"
+        v-model="id"
       />
       <label for="floatingInput">아이디</label>
     </div>
@@ -23,11 +24,14 @@
         class="form-control"
         id="floatingPassword"
         placeholder="비밀번호"
+        v-model="password"
       />
       <label for="floatingPassword">비밀번호</label>
     </div>
 
-    <button class="w-100 btn btn-lg btn-primary">로그인</button>
+    <button type="button" class="w-100 btn btn-lg btn-primary" @click="login">
+      로그인
+    </button>
     <router-link :to="{ name: 'join' }">
       <span class="span-inline">회원가입</span>
     </router-link>
@@ -36,9 +40,33 @@
     </router-link>
   </form>
 </template>
-
 <script>
-export default {};
+import { mapActions, mapState } from "vuex";
+
+const userStore = "userStore";
+export default {
+  name: "UserLogin",
+  data() {
+    return {
+      id: null,
+      password: null,
+    };
+  },
+  computed: {
+    ...mapState(userStore, ["isLogin", "isLoginError", "userInfo"]),
+  },
+  methods: {
+    ...mapActions(userStore, ["userConfirm"]),
+    async login() {
+      await this.userConfirm({
+        id: this.id,
+        secret: this.password,
+      });
+
+      this.$router.push({ name: "main" });
+    },
+  },
+};
 </script>
 
 <style scoped>
--- happyhouse_fe/src/store/index.js
+++ happyhouse_fe/src/store/index.js
@@ -2,13 +2,15 @@ import Vue from "vue";
 import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
 // modules
-import aptStore from "@/store/modules/AptStore.vue";
+import aptStore from "@/store/modules/AptStore.js";
+import userStore from "@/store/modules/UserStore.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     aptStore,
+    userStore,
   },
   plugins: [
     createPersistedState({
