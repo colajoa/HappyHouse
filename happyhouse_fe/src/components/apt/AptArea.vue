ca9f6712034037c807dc787a009ed379242053c1
jun <ktf1406@naver.com>
2022-11-19 00:22:27 +0900

    [Feat] Modify Button Color

--- happyhouse_fe/src/components/apt/AptArea.vue
+++ happyhouse_fe/src/components/apt/AptArea.vue
@@ -57,7 +57,7 @@
           </select>
         </td>
         <td>
-          <button type="button" class="btn btn-outline-primary">검색</button>
+          <button type="button" class="btn btn-custom">검색</button>
         </td>
       </tr>
     </table>
@@ -92,6 +92,17 @@ td select {
 
 td button {
   margin: 0 20px;
+}
+
+.btn-custom {
+  --bs-btn-font-weight: 600;
+  --bs-btn-color: white;
+  --bs-btn-bg: #67b0d1; /*연한거*/
+  --bs-btn-border-radius: 0.5rem;
+  --bs-btn-hover-color: white;
+  --bs-btn-hover-bg: #409cc5;
+  --bs-btn-active-color: var(--bs-btn-color);
+  --bs-btn-active-bg: var(--bs-btn-bg);
 }
 </style>
 >
