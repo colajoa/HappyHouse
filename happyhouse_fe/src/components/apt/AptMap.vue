4a0c44aab30599150cc3316362cab9db77698375
jun <ktf1406@naver.com>
2022-11-21 02:07:12 +0900

    [Feat] Modify Map

--- happyhouse_fe/src/components/apt/AptMap.vue
+++ happyhouse_fe/src/components/apt/AptMap.vue
@@ -46,15 +46,16 @@ export default {
 <style>
 #map {
   /* display: none; */
-  /* width: 100%;
-  height: 100vh; */
-
+  position: absolute;
   width: 100%;
+  height: 100%;
+
+  /* width: 100%;
   height: 100%;
   position: absolute;
   left: 0;
   top: 0;
   margin: 0;
-  padding: 0;
+  padding: 0; */
 }
 </style>
