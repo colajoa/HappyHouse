87c3d7e1e5dd90d27fe06eb5554be7ee45a353a5
jungeun <kjjee99@naver.com>
2022-11-21 14:28:00 +0900

    [Refactor] 토큰 수정

--- happyhouse_fe/src/api/axios.js
+++ happyhouse_fe/src/api/axios.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
+let token = sessionStorage.getItem("token");
+if (token == null) token = "";
+
 // axios 객체 생성
 export default axios.create({
   baseURL: "http://localhost:8888",
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
-    Authorization: "",
+    Authorization: token,
   },
   withCredentials: true,
 });
