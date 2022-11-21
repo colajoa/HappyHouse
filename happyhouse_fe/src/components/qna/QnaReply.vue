76d8934ff341880131bd8f6f107defdac3cd672b
jun <ktf1406@naver.com>
2022-11-21 15:16:29 +0900

    [Feat] Modify QnA Page

---
+++ happyhouse_fe/src/components/qna/QnaReply.vue
@@ -0,0 +1,24 @@
+<template>
+  <tr v-if="qna.isReply">
+    <td>A</td>
+    <td colspan="4">{{}}</td>
+  </tr>
+  <tr v-else>
+    <td>A</td>
+    <td colspan="4">답변 등록이 되지 않았습니다.</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "QnaReply",
+  data() {
+    return {};
+  },
+  props: {
+    qnaid: Number,
+  },
+};
+</script>
+
+<style></style>
