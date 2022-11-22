6024fc1cd077837ad1aab0c7e261e69ab9669103
jungeun <kjjee99@naver.com>
2022-11-22 10:44:27 +0900

    [Feat] Board 관련 함수 구현

---
+++ happyhouse_fe/src/store/modules/BoardStore.js
@@ -0,0 +1,30 @@
+import http from "@/api/axios.js";
+
+const BoardStore = {
+  namespaced: true,
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {
+    async boardList(state) {
+      return await http.get("/house/board/list");
+    },
+    async post(state, board) {
+      await http.post("/house/board/register", board);
+    },
+    async detailBoard(state, idx) {
+      await http.get(`/house/board/detail/${idx}`);
+    },
+    async viewBoard(state, idx) {
+      await http.get(`/house/board/hit/{${idx}}`);
+    },
+    async modifyBoard(state, board) {
+      await http.put(`/house/board/${board.idx}`, board);
+    },
+    async removeBoard(state, idx) {
+      await http.delete(`/house/board/${idx}`);
+    },
+  },
+};
+
+export default BoardStore;
