7accb75acd85bde2d74eb56a405cc3252e33a259
jungeun <kjjee99@naver.com>
2022-11-23 12:39:26 +0900

    [Fix] 오류 수정

--- happyhouse/src/main/java/com/ssafy/happyhouse/controller/AptController.java
+++ happyhouse/src/main/java/com/ssafy/happyhouse/controller/AptController.java
@@ -1,15 +1,22 @@
 package com.ssafy.happyhouse.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.happyhouse.dto.AptDeal;
 import com.ssafy.happyhouse.dto.CodeDto;
 import com.ssafy.happyhouse.service.AptServiceImpl;
 
@@ -17,11 +24,14 @@ import com.ssafy.happyhouse.service.AptServiceImpl;
 @RequestMapping("/apt")
 public class AptController {
 
+    @Value("${APT_API_KEY}")
+    private static String API_KEY;
+
     @Autowired
     private AptServiceImpl aptService;
 
     // 시도 목록 불러오기
-    @GetMapping("/sido/")
+    @GetMapping("/sido")
     public ResponseEntity<?> getSido() throws Exception{
         List<CodeDto> list = aptService.getSido(null);
 
@@ -63,13 +73,39 @@ public class AptController {
      * @param dealMonth
      * @return 
      */
-    @GetMapping("/aptlist/{code}/{dealYear}/{dealMonth}")
+    @GetMapping("/aptlist/{code}/{date}")
     public ResponseEntity<?> getAptList(@PathVariable("code") String code,
-        @PathVariable("dealYear") int dealYear,
-        @PathVariable("dealMonth") int dealMonth) throws Exception{
+        @PathVariable("date") String date) throws Exception{
         
-        List<AptDeal> list = aptService.getAptList(code, dealYear, dealMonth);
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + API_KEY); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("LAWD_CD","UTF-8") + "=" + URLEncoder.encode(code, "UTF-8")); /*지역코드*/
+        urlBuilder.append("&" + URLEncoder.encode("DEAL_YMD","UTF-8") + "=" + URLEncoder.encode(date, "UTF-8")); /*계약월*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+        // System.out.println(sb.toString());
 
-        return ResponseEntity.ok(list);
+        JSONObject json = XML.toJSONObject(sb.toString());
+        System.out.println(json);
+        // return ResponseEntity.ok(list);
+        return ResponseEntity.ok(json.toMap());
     }
 }
