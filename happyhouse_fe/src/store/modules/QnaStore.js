c87f9f0821cc35c5b053b1f795bd6b9b9f2b58a8
jungeun <kjjee99@naver.com>
2022-11-24 16:46:02 +0900

    [Feat] QnA 답변 등록, 수정, 삭제 함수 구현

--- happyhouse_fe/src/store/modules/QnaStore.js
+++ happyhouse_fe/src/store/modules/QnaStore.js
@@ -53,6 +53,9 @@ const QnaStore = {
     async removeQna(state, idx) {
       await http.delete(`/house/qna/${idx}`);
     },
+    async registerQnaReply(state, reply) {
+      await http.post("/house/qna/reply/register", reply);
+    },
     async getQnaReply({ commit }, idx) {
       await http.get(`/house/qna/reply/detail/${idx}`).then((res) => {
         if (res.status == 200) {
@@ -61,6 +64,7 @@ const QnaStore = {
       });
     },
     async modifyQnaReply(state, reply) {
+      console.log(reply);
       await http.put(`/house/qna/reply/${reply.qnaid}`, reply);
     },
     async removeQnaReply(state, idx) {
